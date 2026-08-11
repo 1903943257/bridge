c73e464e02df8cc5ad7c38883854cf5562f5eaf8
1903943257 <1903943257@users.noreply.github.com>
2026-08-11 22:01:07 +0800

    feat: add DTA ModuleSpec replacer

--- verl/models/mcore/dta/__init__.py
+++ verl/models/mcore/dta/__init__.py
@@ -16,6 +16,7 @@
 
 from .attention import DTASelfAttention
 from .context import TreeAttentionContext, get_tree_attention_context, use_tree_attention_context
+from .module_spec import replace_self_attention_with_dta
 from .rectangular_attention import rectangular_causal_attention
 from .rope import build_suffix_rotary_pos_emb
 
@@ -25,5 +26,6 @@ __all__ = [
     "build_suffix_rotary_pos_emb",
     "get_tree_attention_context",
     "rectangular_causal_attention",
+    "replace_self_attention_with_dta",
     "use_tree_attention_context",
 ]
